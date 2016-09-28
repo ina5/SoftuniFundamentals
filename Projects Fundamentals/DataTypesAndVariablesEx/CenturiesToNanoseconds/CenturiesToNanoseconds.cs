c88a131208a949fb622c1f9946cdf35827bcdfa8
ina5 <inapanova5@gmail.com>
2016-09-28 10:18:14 +0300

    Added 2 more exercises

--- Projects Fundamentals/DataTypesAndVariablesEx/CenturiesToNanoseconds/CenturiesToNanoseconds.cs
+++ Projects Fundamentals/DataTypesAndVariablesEx/CenturiesToNanoseconds/CenturiesToNanoseconds.cs
@@ -6,10 +6,21 @@ using System.Threading.Tasks;
 
 namespace CenturiesToNanoseconds
 {
-    class Program
+    class CenturiesToNanoseconds
     {
         static void Main(string[] args)
         {
+            uint centuries = uint.Parse(Console.ReadLine());
+            uint years = centuries * 100;
+            uint days = (uint)(years * 365.2422);
+            uint hours = days * 24;
+            decimal minutes = hours * 60M;
+            decimal seconds = minutes * 60M;
+            decimal milliseconds = seconds * 1000M;
+            decimal microseconds = milliseconds * 1000;
+            decimal nanoseconds = microseconds * 1000;
+            Console.WriteLine("{0} centuries = {1} years = {2} days = {3} hours = {4} minutes = {5} seconds = {6} milliseconds = {7} microseconds = {8} nanoseconds", centuries, years, days, hours, minutes, seconds, milliseconds, microseconds, nanoseconds);
+
         }
     }
 }
--- Projects Fundamentals/DataTypesAndVariablesEx/ConvertSpeedUnits/ConvertSpeedUnits.cs
+++ Projects Fundamentals/DataTypesAndVariablesEx/ConvertSpeedUnits/ConvertSpeedUnits.cs
@@ -10,7 +10,13 @@ namespace ConvertSpeedUnits
     {
         static void Main(string[] args)
         {
-
+            int distance = int.Parse(Console.ReadLine());
+            int hours= int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            int seconds = int.Parse(Console.ReadLine());
+            float metersSec = distance / seconds;
+            float kmHour = (distance / 1000) / hours;
+                float milesHour
         }
     }
 }
